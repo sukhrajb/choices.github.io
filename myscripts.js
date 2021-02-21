e6160c75708964833523fe66930959146b183f52
sukhrajb <78326492+sukhrajb@users.noreply.github.com>
2021-02-20 22:55:23 -0500

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,30 @@
 function update(e){
-    var x = e.clientX || e.touches[0].clientX
-    var y = e.clientY || e.touches[0].clientY
-  
-    document.documentElement.style.setProperty('--cursorX', x + 'px')
-    document.documentElement.style.setProperty('--cursorY', y + 'px')
-  }
-  
-  document.addEventListener('mousemove',update)
-  document.addEventListener('touchmove',update)
-  var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
 
-function toggleDoor() {
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+
+
+function toggleDoor(element) {
   element.classList.toggle("doorOpen");
 }
-var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
 
-function toggleDoor() {
+function goToPage1(element) {
   element.classList.toggle("doorOpen");
+  setTimeout(function(){
+    window.open('page1.html', 'mywindow');
+  }, 1000);
 }
+
+  
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
+
+window.addEventListener('load', function(){
+  var element = document.querySelector(".door.page1door");
+  element.addEventListener("click", goToPage1.bind(event, element));
+
+});
+
