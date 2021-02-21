68fce1264b9e1e8db08e33bac32946e78eda5556
sukhrajb <78326492+sukhrajb@users.noreply.github.com>
2021-02-20 21:11:11 -0500

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -14,3 +14,9 @@ element.addEventListener("click", toggleDoor);
 function toggleDoor() {
   element.classList.toggle("doorOpen");
 }
+var element = document.querySelector(".door");
+element.addEventListener("click", toggleDoor);
+
+function toggleDoor() {
+  element.classList.toggle("doorOpen");
+}
